a96a5c8c6e6c5ac54d374029b6a036affccb3479
Leandro Cardoso <leandrocardoso@gmail.com>
2013-10-11 10:07:23 -0300

    login.sql added comments

--- login.sql
+++ login.sql
@@ -1,8 +1,13 @@
 --set linesize 213
 set linesize 32767
 set pagesize 32767
+
+-- disable the header underline
 set underline off
+
+-- use spaces to format white space
 set tab off
+
 set feed off
 alter session set nls_date_format='dd/mm/yyyy HH24:mi:ss';
 set feed on
